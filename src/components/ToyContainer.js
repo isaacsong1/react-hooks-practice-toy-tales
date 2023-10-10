4ef1da92480538f76eb7d244a20b227a6e0460fe
Isaac Song <isaacsongs10@gmail.com>
2023-10-10 16:56:50 -0700

    Render toys to page

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys}) {
+  const mappedToys = toys.map(toy => <ToyCard key={toy.id} {...toy} />)
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{mappedToys}</div>
   );
 }
 
